0a255c6b5f3685badf620775191fabb49a969460
Talha Yücel <talha.yuecel@student.htldornbirn.at>
2025-03-13 15:28:24 +0100

    Dritte Aufgabe gemacht

--- Aufgaben/Aufgaben_OneNote_Loops/Aufgabe3.js
+++ Aufgaben/Aufgaben_OneNote_Loops/Aufgabe3.js
@@ -5,3 +5,17 @@
 // "FizzBuzz" ausgibt, wenn sie durch 3 und 5 teilbar ist,
 // ansonsten die Zahl selbst ausgibt.
 
+for (let i = 0; i <= 50; i++) {
+    if (i % 3 === 0) {
+        console.log("Fizz")
+    }
+    else if (i % 5 === 0) {
+        console.log("Buzz")
+    }
+    else if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz")
+    }
+    else {
+        console.log(i)
+    }
+}
