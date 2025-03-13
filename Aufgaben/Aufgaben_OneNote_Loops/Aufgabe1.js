7a48c20864216c84535f383be53ae5643c581416
Talha Yücel <talha.yuecel@student.htldornbirn.at>
2025-03-13 15:07:30 +0100

    Aufgabe 2 gemacht

---
+++ Aufgaben/Aufgaben_OneNote_Loops/Aufgabe1.js
@@ -0,0 +1,7 @@
+// Gerade Zahlen filtern
+for (let i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
---
+++ Aufgaben/Aufgaben_OneNote_Loops/Aufgabe2.js
@@ -0,0 +1,8 @@
+//Summe aller ungeraden Zahlen von 1 bis 100
+let sum = 0;
+
+for (let i = 1; i <= 100; i += 2) {
+    sum += i;
+}
+
+console.log(sum)
