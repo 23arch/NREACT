f6a67449553d08ad61d925b4233b0a85cc4860bc
Talha Yücel <talha.yuecel@student.htldornbirn.at>
2025-03-13 15:37:45 +0100

    Aufgabe 6 gemacht

--- Aufgaben/Aufgaben_OneNote_Loops/Aufgabe6.js
+++ Aufgaben/Aufgaben_OneNote_Loops/Aufgabe6.js
@@ -1,3 +1,11 @@
 //Gegeben sei eine Schleife, die true und false für jede Zahl von 1 bis 20 ausgibt.
 //Verwende eine logische Operation, um "true" für gerade Zahlen und "false" für ungerade Zahlen auszugeben.
 
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i, "true")
+    }
+    else {
+        console.log(i, "false")
+    }
+}
