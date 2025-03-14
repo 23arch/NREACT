b69c635b9289f5c53aca6f6e850ce74e7ac41674
Talha Yücel <talha.yuecel@student.htldornbirn.at>
2025-03-14 12:50:58 +0100

    Aufgabe 7 erledigt

--- Aufgaben/Aufgaben_OneNote_Loops/Aufgabe4.js
+++ Aufgaben/Aufgaben_OneNote_Loops/Aufgabe4.js
@@ -1,2 +1,6 @@
 //Schreibe eine Schleife, die alle Primzahlen zwischen 2 und 50 ausgibt. Eine Primzahl ist nur durch 1 und sich selbst teilbar.
 
+
+for (i = 2; i <= 50; i++) {
+
+}
--- Aufgaben/Aufgaben_OneNote_Loops/Aufgabe7.js
+++ Aufgaben/Aufgaben_OneNote_Loops/Aufgabe7.js
@@ -1,1 +1,10 @@
 // Gib die Zahlen von 1 bis 50 aus. Wenn eine Zahl durch 5 teilbar ist, gib statt der Zahl "Divisible by 5" aus
+
+for (i = 1; i <= 50; i++) {
+    if (i % 5 === 0) {
+        console.log("Divisible by 5")
+    } else {
+        console.log(i)
+    }
+
+}
