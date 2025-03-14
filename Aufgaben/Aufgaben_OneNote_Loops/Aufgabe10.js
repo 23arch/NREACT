ea8e6dbd179bf5fa80ba24f84742373e54e3d88d
Talha Yücel <talha.yuecel@student.htldornbirn.at>
2025-03-14 13:20:51 +0100

    Aufgabe 10 gemacht

--- Aufgaben/Aufgaben_OneNote_Loops/Aufgabe10.js
+++ Aufgaben/Aufgaben_OneNote_Loops/Aufgabe10.js
@@ -3,3 +3,16 @@
 // Wenn sie nur durch 3 teilbar ist, gib "Divisible by 3" aus.
 // Wenn sie nur gerade ist, gib "Even" aus.
 // Ansonsten gib die Zahl selbst aus.
+
+for (i = 1; i <= 30; i++) {
+    if (i % 2 === 0 && i % 3 === 0) {
+        console.log("Even & Divisible by 3")
+    }
+    else if (i % 3 === 0) {
+        console.log("Divisible by 3")
+    } else if (i % 2 === 0) {
+        console.log("Even")
+    } else {
+        console.log(i)
+    }
+}
