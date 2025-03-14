de60ac3470b5fae70846abc036930f601a95582b
Talha Yücel <talha.yuecel@student.htldornbirn.at>
2025-03-14 12:55:08 +0100

    Aufgabe 8 erledigt

--- Aufgaben/Aufgaben_OneNote_Loops/Aufgabe8.js
+++ Aufgaben/Aufgaben_OneNote_Loops/Aufgabe8.js
@@ -1,1 +1,9 @@
 //Gehe alle Zahlen von 1 bis 50 durch und zähle, wie viele davon durch 7 teilbar sind.
+let count = 0;
+
+for (i = 1; i <= 50; i++) {
+    if (i % 7 === 0) {
+        count += 1
+    }
+}
+console.log(count)
