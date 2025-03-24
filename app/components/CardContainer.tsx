99e584407d0554dadc972fff0f595cd58cdf1204
Talha Yücel <talha.yuecel@student.htldornbirn.at>
2025-03-24 07:42:45 +0100

    Fehlende Syntax

--- app/components/CardContainer.tsx
+++ app/components/CardContainer.tsx
@@ -1,18 +1,34 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import Card from './ui/cars/Card';
+import { log } from 'console';
 
 type Props = {}
 
 export default function CardContainer({ }: Props) {
     const [todos, setTodos] = useState([]);
+    const [todosToShow, setTodosToShow] = useState([]);
+    const [showtTop3, setShowTop3] = useState(false);
 
     useEffect(() => {
-        console.log("in Effect");
+        const fetchData = () => {
+            fetch("").then(
+                (result: any) => result.json().then((data: any) => {
+                    console.log(data);
+                    setTodos(data);
+                    setTodosToShow(data);
+                }))
+        };
+        fetchData();
     }, []);
+
+    useEffect(() => {
+        alert("change");
+    }, [showtTop3]);
     return (
         <div>
-            {todos.map((todo: any) => {
+            <button onClick={() => setShowTop3(!showtTop3)}>Filter</button>
+            {todosToShow.map((todo: any) => {
                 return <Card title={todo.title} description={todo.description} />;
             })}
         </div>
